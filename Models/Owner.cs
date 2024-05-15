7f05593d818ecc831efffa82f7bc10ae9e918512
Fells <micheldcalil@hotmail.com>
2024-05-14 23:35:59 -0300

    Created One to many Relation between the Models

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -8,5 +8,7 @@
         public string Name { get; set; }
 
         public string Gym { get; set; }
+
+        public Country Country { get; set; }
     }
 }
--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -8,5 +8,7 @@
         public string Name { get; set; }
 
         public DateTime BirthDate { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
     }
 }
