7aa4845f2e750022d08afff10b9ea7c83e41b1a7
Fells <micheldcalil@hotmail.com>
2024-05-20 00:46:25 -0300

    Created all of the remaining GET methods to the Reviewer Table DTO/Helper/Interfaces/Repository/Controller

---
+++ Repository/ReviewerRepository.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly ConnectionContext _reviewerContext;
+        private readonly IMapper _mapper;
+
+        public ReviewerRepository(ConnectionContext context, IMapper mapper)
+        {
+            _reviewerContext = context;
+            _mapper = mapper;
+        }
+
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _reviewerContext.Reviewers.ToList(); 
+        }
+
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            return _reviewerContext.Reviewers.Where(r => r.Id == reviewerId).Include(e => e.Reviews).FirstOrDefault();
+        }
+
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            return _reviewerContext.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            return _reviewerContext.Reviewers.Any(r => r.Id == reviewerId);
+        }
+    }
+}
