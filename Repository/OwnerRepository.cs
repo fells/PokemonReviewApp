7f9bc54ad8d2da656de3dd6ec6afe5016e38df0e
Fells <micheldcalil@hotmail.com>
2024-05-19 23:06:27 -0300

    Finished the Owner GET methods, DTO/Helper/Interface.Repository and Controller

---
+++ Repository/OwnerRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private ConnectionContext _connectionContext;
+
+        public OwnerRepository(ConnectionContext context)
+        {
+            _connectionContext = context;
+        }
+
+        public ICollection<Owner> GetOwners()
+        {
+           return _connectionContext.Owners.ToList();
+        }
+        public Owner GetOwner(int ownerId)
+        {
+            return _connectionContext.Owners.Where(o => o.Id == ownerId).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnerOfAPokemon(int pokeId)
+        {
+            return _connectionContext.PokemonOwners.Where(p => p.Pokemon.Id == pokeId).Select(o => o.Owner).ToList();
+        }
+
+
+        public ICollection<Pokemon> GetPokemonByOwner(int ownerId)
+        {
+            return _connectionContext.PokemonOwners.Where(p => p.Owner.Id == ownerId).Select(p => p.Pokemon).ToList();
+        }
+
+        public bool OwnerExists(int ownerId)
+        {
+            return _connectionContext.Owners.Any(o => o.Id == ownerId);
+        }
+    }
+}
